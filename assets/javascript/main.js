6d11fa6379decc415b62f7110159acc06f58d373
defaultboy <mj.muchlinski@gmail.com>
2015-07-07 11:42:19 +0100

    Total pledged now updating correctly, added number formatting and some tweaks on the pledge form.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,21 @@
+// select elements to modify
+var pageTitle = document.getElementById('page-title'),
+	mainHeading = document.getElementById('main-heading'),
+	story = document.getElementById('story'),
+	totalPledged = document.getElementById('total-pledged'),
+	target = document.getElementById('target'),
+	ownerName = document.getElementById('owner-name'),
+	percentPledged = document.getElementById('percent-pledged'),
+	progressBar	= document.getElementById('progress-bar'),
+	pledgeForm = document.getElementById('pledge-form'); 
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function ajaxRequest(){
  var activexmodes = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP'] //activeX versions to check for in IE
- if (window.ActiveXObject){ //Test for support for ActiveXObject in IE first (as XMLHttpRequest in IE7 is broken)
+ if (window.ActiveXObject){ //test for support for ActiveXObject in IE first (as XMLHttpRequest in IE7 is broken)
  	for (var i = 0; i < activexmodes.length; i++){
  		try{
  			return new ActiveXObject(activexmodes[i])
@@ -10,6 +25,7 @@ function ajaxRequest(){
 		}
 	}
 }
+
  else if (window.XMLHttpRequest) // if Mozilla, Safari etc
  	return new XMLHttpRequest()
  else
@@ -28,48 +44,27 @@ function populatePage(){
    		var jsondata = eval('('+mygetrequest.responseText+')') //retrieve result as a JavaScript object
    
 		   // populate page content
-		   document.getElementById('page-title').innerHTML = 'JustGiving - ' + jsondata.name;
-		   document.getElementById('main-heading').innerHTML = jsondata.name;
-		   document.getElementById('story').innerHTML = jsondata.story;
-		   document.getElementById('total-pledged').innerHTML = '&pound;' + jsondata.totalPledged;
-		   document.getElementById('target').innerHTML = 'pledged of <strong>&pound;' + jsondata.target + '</strong> funding target';
-		   document.getElementById('owner-name').innerHTML = jsondata.owner;
+		   pageTitle.innerHTML = 'JustGiving - ' + jsondata.name;
+		   mainHeading.innerHTML = jsondata.name;
+		   story.innerHTML = jsondata.story;
+
+		   var jsonTotalPledged = jsondata.totalPledged.toFixed(2);
+
+		   totalPledged.innerHTML = '&pound;' + numberWithCommas(jsonTotalPledged);
+		   target.innerHTML = 'pledged of <strong>&pound;' + numberWithCommas(jsondata.target) + '</strong> funding target';
+		   ownerName.innerHTML = jsondata.owner;
 
 		   // calculate percentage
-		   var percentPledged = Math.floor((jsondata.totalPledged / jsondata.target) * 100);
-		   document.getElementById('percent-pledged').innerHTML = percentPledged + '%';
+		   var percentPledgedValue = Math.floor((jsondata.totalPledged / jsondata.target) * 100);
+		   percentPledged.innerHTML = percentPledgedValue + '%';
 
 		   // update progress bar
-		   document.getElementById('progress-bar').value = percentPledged;
+		   progressBar.value = percentPledgedValue;
 
 		}
 		else{
-			alert('Sorry there appears to have been an error. Please try again by refreshing the page.')
+			alert('Sorry, something went wrong. Please try again by refreshing the page.')
 			}
-	}
-}
-
-// update progress bar and pledged amount after new pledge is submitted
-function updateProgress(){
-	if (mygetrequest.readyState == 4){
-
-		if (mygetrequest.status == 200 || window.location.href.indexOf('http') == -1){
-   		var jsondata = eval('('+mygetrequest.responseText+')') //retrieve result as a JavaScript object
-
-	   /* update total */
-	   document.getElementById('total-pledged').innerHTML = '&pound;' + jsondata.totalPledged;
-
-	   // calculate percentage
-	   var percentPledged = Math.floor((jsondata.totalPledged / jsondata.target) * 100);
-	   document.getElementById('percent-pledged').innerHTML = percentPledged + '%';
-
-	   // update progress bar
-	   document.getElementById('progress-bar').value = percentPledged;
-
-		}
-		else{
-			alert('An error has occured making the request')
-		}
 	}
 }
 
@@ -79,6 +74,7 @@ function postPledge(pledgeForm, amount) {
 	console.log('posting');
 
 	// add loading class to form
+	var pledgeFormDefaultClass = pledgeForm.className;
 	pledgeForm.className = pledgeForm.className + " loading";
 
 	var mypostrequest = new ajaxRequest();
@@ -95,18 +91,45 @@ function postPledge(pledgeForm, amount) {
 			console.log(mypostrequest.status);
 
 			if (mypostrequest.status === 200) {
-				console.log('thank you');
+				console.log('post success');
 
-				pledgeForm.className = pledgeForm.className + ' success';
+				pledgeForm.className = pledgeFormDefaultClass + ' success';
 				pledgeForm.innerHTML = '<span class="thank-you-message">Thanks for your pledge!</span>';
 
-				var mygetrequest = new ajaxRequest();
-				mygetrequest.onreadystatechange = updateProgress;
-				mygetrequest.open('GET', '/api/crowdFundingPage');
-				mygetrequest.send(null);
+				// update totals and progress bar
+				var myupdaterequest = new ajaxRequest()
+				myupdaterequest.onreadystatechange = updateTotals;
+				myupdaterequest.open('GET', '/api/crowdFundingPage', true);
+				myupdaterequest.send(null);
+
+				function updateTotals(){
+				if (myupdaterequest.readyState == 4){
+
+				if (myupdaterequest.status == 200 || window.location.href.indexOf('http') == -1){
+   				var jsondata = eval('('+myupdaterequest.responseText+')') //retrieve result as a JavaScript object
+   
+			   // populate page content
+			   var jsonTotalPledged = jsondata.totalPledged.toFixed(2);
+
+			   totalPledged.innerHTML = '&pound;' + numberWithCommas(jsonTotalPledged);
+
+			   // calculate percentage
+			   var percentPledgedValue = Math.floor((jsondata.totalPledged / jsondata.target) * 100);
+			   percentPledged.innerHTML = percentPledgedValue + '%';
+
+			   // update progress bar
+			   progressBar.value = percentPledgedValue;
+
+			}
+		else{
+			alert('Sorry, something went wrong. Please try again by refreshing the page.')
+			}
+	}
+}
 
 			} else {
-				alert('There was a problem with the request.');
+				pledgeForm.className = pledgeFormDefaultClass;
+				alert('Sorry, there was a problem submitting your pledge. Please try again.');
 			}
 		}		
 	}
@@ -114,20 +137,22 @@ function postPledge(pledgeForm, amount) {
 } 
 
 // validate form abd post pledge to database
-var pledgeForm = document.getElementById('pledge-form'); 
-
 pledgeForm.addEventListener('submit', function(e){
 
 	var amount = pledgeForm.elements['pledge-amount'].value;
+		//remove pound sign
+		amount = amount.replace(/\u00A3/g, '');
 
-  // validate input
-  if( amount != '') {
+  // check if not null empty or not a number
+  if( !amount || isNaN(amount) ) {
 
-  	postPledge(pledgeForm, amount);
+  	alert('please enter a number');
 
   }
   else {
-  	alert('please enter a number');
+
+  	postPledge(pledgeForm, amount);
+
   }
 
   e.preventDefault(); 
